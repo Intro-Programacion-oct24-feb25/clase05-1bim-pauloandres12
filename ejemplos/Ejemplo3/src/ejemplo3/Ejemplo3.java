8dbcfa1eb9b795791657c8da3a2f156d7d5cf61f
Paulo Andres <pbustamante@utpl.edu.ec>
2024-11-06 12:22:49 -0500

    Actualizacion 4

--- ejemplos/Ejemplo3/src/ejemplo3/Ejemplo3.java
+++ ejemplos/Ejemplo3/src/ejemplo3/Ejemplo3.java
@@ -30,7 +30,7 @@ public class Ejemplo3 {
             System.out.printf("Estudiante aprobado con un "
                     + "promedio: %.2f\n", promedio);
         } else {
-            if ((promedio >= 5) && (promedio < 7.5)) {
+            if ((promedio >= 5) || (promedio < 7.5)) { //"&&" and "||" or
                 System.out.printf("Estudiante en suspenso con un "
                         + "promedio: %.2f\n", promedio);
             } else {
